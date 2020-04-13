2f672ae8c82aae13440a38f17b3a1e8247afb49d
Barri <info@bdesignstudio.co.uk>
2020-04-13 17:59:04 +0100

    Make senarios for advancer version

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-alert("It is zombie apocalypse. You are looting a store and suddenly a zombie bursts through the door!");
-var weapon = prompt("You search around frantically for a weapon. What do you choose? A bow and arrow, an axe , or a rubber chicken?");
+var beginningSenario = [" You wake up in a hospital. It is quiet. You tiptoe to the door and peek out.", "You are standing in an open field west of a white house with a boarded front door. There is a small mailbox here.", "Desperate times call for desperate measures. You see a small convenience store up ahead and decide to loot it for goods."];
 
-var rand = Math.round( Math.random());
-
-alert("You attack the zombie with your "+ weapon);
-
-if (rand === 0 ){
-  alert("The zombie bites you. You lose!!!")
-} else if(rand ===1) {
-  alert("You kill the zombie with your "+ weapon + ". You win!!!")
+function randNum(range) {
+ return Math.round(Math.random() * range);
 }
+alert(beginningSenario[randNum(beginningSenario.length -1)]);
+
+
+// var weapon = prompt("You search around frantically for a weapon. What do you choose? A bow and arrow, an axe , or a rubber chicken?");
+
+// var rand = Math.round(Math.random());
+
+// alert("You attack the zombie with your " + weapon);
+
+// if (rand === 0) {
+//   alert("The zombie bites you. You lose!!!")
+// } else if (rand === 1) {
+//   alert("You kill the zombie with your " + weapon + ". You win!!!")
+// }
