c6f109165d51430540362c13d253859485f2ff62
Barri <info@bdesignstudio.co.uk>
2020-04-13 12:45:32 +0100

    Attack the zombie

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 alert("It is zombie apocalypse. You are looting a store and suddenly a zombie bursts through the door!");
 var weapon = prompt("You search around frantically for a weapon. What do you choose? A bow and arrow, an axe , or a rubber chicken?");
 
+var rand = Math.round( Math.random());
+
 alert("You attack the zombie with your "+ weapon);
+
+if (rand === 0 ){
+  alert("The zombie bites you. You lose!!!")
+} else if(rand ===1) {
+  alert("You kill the zombie with your "+ weapon + ". You win!!!")
+}
