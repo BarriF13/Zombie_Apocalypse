04197f556b60ed473e9964921a876119eeb0e14b
Barri <info@bdesignstudio.co.uk>
2020-04-13 18:10:23 +0100

    Final result

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ alert(beginningSenario[randNum(beginningSenario.length -1)]);
  var weapon = weaponList[ randNum(weaponList.length -1)];
  alert("Being that it is the zombie apocalypse, you decide to search for a weapon first. After surveying your surroundings you notice and grab a " + weapon+ ".")
 
+ alert("Suddenly a zombie burts through the door! You ready your " + weapon + " and advance towards the zombie");
 
-
-// alert("You attack the zombie with your " + weapon);
-
-// if (rand === 0) {
-//   alert("The zombie bites you. You lose!!!")
-// } else if (rand === 1) {
-//   alert("You kill the zombie with your " + weapon + ". You win!!!")
-// }
+ var survival = randNum(2);
+if (survival === 0) {
+ alert("The zombie bites you. You lose!!!")
+ } else if (survival > 0) {
+  alert("You kill the zombie with your " + weapon + ". You win!!!")
+ }
