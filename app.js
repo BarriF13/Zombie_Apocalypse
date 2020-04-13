0fd11c04d5bf0840cd2db59729f3594aa572f435
Barri <info@bdesignstudio.co.uk>
2020-04-13 12:39:25 +0100

    Give weaopon's choices

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("It is zombie apocalypse. You are looting a store and suddenly a zombie bursts through the door!");
+var weapon = prompt("You search around frantically for a weapon. What do you choose? A bow and arrow, an axe , or a rubber chicken?");
+
+alert("You attack the zombie with your "+ weapon);
