c43a3cb9ebe358bf0b77b76d6a8019f331e9859a
Barri <info@bdesignstudio.co.uk>
2020-04-14 17:02:09 +0100

    Level up add name and character to player

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+var outcome;
+
+window.alert("one day you wake up and the world as you know has ended. The dead come to life and pray on the living. There is no hope, no cure, only survuval. Welcome to the zombie apocalypse.");
+
 var beginningSenario = [" You wake up in a hospital. It is quiet. You tiptoe to the door and peek out.", "You are standing in an open field west of a white house with a boarded front door. There is a small mailbox here.", "Desperate times call for desperate measures. You see a small convenience store up ahead and decide to loot it for goods."];
 
 function randNum(range) {
- return Math.round(Math.random() * range);
+  "use strict";
+  if (typeof range === "number"){
+    return Math.round(Math.random() * range);
+  }
 }
-alert(beginningSenario[randNum(beginningSenario.length -1)]);
+window.alert(beginningSenario[randNum(beginningSenario.length -1)]);
 
+  var character = {
+    health: 5,
+    strenght: 0,
+    stealth: 0,
+    name: window.prompt("What is your name?"),
+    characterClass: window.prompt("These times test the strenghts of our character. What were you before the war? ( Choose from: soldier, doctor, or artist)").toLowerCase()
 
+  };
+
+  if(!character.name) {
+    character.name = window.prompt("I don't know what to call you if you don't enter a name. if you don't answer I name you myself hahaha....., what is your name???");
+    if (!character.name) {
+       window.alert(" Well I did try to warn you. Gunther Piddles it is then.");
+       character.name = " Gunther Piddles ";
+    }
+  }
+  
  var weaponList = ["shovel" , "toothbrush","axe", "crossbow","Rubber chicken"];
 
  var weapon = weaponList[ randNum(weaponList.length -1)];
