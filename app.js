cda8d6cbc66478f49c2bf39ffe4ace09bba56699
Barri <info@bdesignstudio.co.uk>
2020-04-14 17:37:04 +0100

    Win or lose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ window.alert("one day you wake up and the world as you know has ended. The dead 
 var beginningSenario = [" You wake up in a hospital. It is quiet. You tiptoe to the door and peek out.", "You are standing in an open field west of a white house with a boarded front door. There is a small mailbox here.", "Desperate times call for desperate measures. You see a small convenience store up ahead and decide to loot it for goods."];
 
 function randNum(range) {
-  "use strict";
+    "use strict";
   if (typeof range === "number"){
     return Math.round(Math.random() * range);
   }
@@ -28,7 +28,54 @@ window.alert(beginningSenario[randNum(beginningSenario.length -1)]);
        character.name = " Gunther Piddles ";
     }
   }
-  
+
+  if (character.characterClass === "soldier")
+  {
+    character.strenght = 5;
+  }
+  if (character.characterClass === "doctor")
+  {
+    character.health = 7;
+  }
+  if (character.characterClass === "artist")
+  {
+    character.stealth = 5;
+  }
+
+var choice = window.prompt("The breave adventurer and former " + character.characterClass + " "+ character.name + "enters a store for some loot. You notice a zombie slowly shuffling around amongst the destruction. What action shall you take? Attack, or attempt to sneak by the zombie?").toLowerCase();
+
+if ( choice === "attack") {
+  if (character.strenght === 5 ) {
+    window.alert("Using only uour bear hands, you slay that zombie! Success! you loot the store for goods and find an axe, a pack of batteries and three can of Beanie Weenies.");
+    outcome = "win";
+    character.strenght++;
+  } else if (character.strenght<= 5) {
+    window.alert("You fearlessly charge zombie with all your strenght. Using your mighty powers you crush thy foe as if it were a sheet of paper you needed to shred.");
+    window.alert("Just kidding. you are immediately bitten by the zombie and die. But not all is lost -- if time spent together makes one friends, then you and the zombie become best friends, and live together for ever. The end. yohohahahaha");
+    outcome = "lose";
+  }
+}
+if (choice === "sneak" || choice === "sneak by the zombie") {
+    if ( character.stealth === 5 ) {
+      window.alert("You sneak by the zombie, you clever bastard.")
+    }
+    if (character.strenght < 5) {
+      window.alert(" You try to slip by the zombie, but accidentally stumble over a can of Beanie Weenies, alerting the zombie. It slowly turns its head and shuffles towards you.");
+      if(character.strenght === 5 ) {
+        window.alert("Although you have alerted the zombie, but you are able to crush its skull with your bare hands.");
+        outcome = "win";
+      } else {
+        window.alert("Weaponless and weak the zombie chomps down on an arm as you fail widly in its general direction.");
+        outcome = "lose";
+      }
+    }
+}
+if (outcome === "lose") {
+  window.alert("You lose!.....");
+} else if(outcome === "win") {
+window.alert("You win, HORAY!!!");
+} 
+/*
  var weaponList = ["shovel" , "toothbrush","axe", "crossbow","Rubber chicken"];
 
  var weapon = weaponList[ randNum(weaponList.length -1)];
@@ -42,3 +89,4 @@ if (survival === 0) {
  } else if (survival > 0) {
   alert("You kill the zombie with your " + weapon + ". You win!!!")
  }
+ */
